a46059dcb47c1c53f5f7b7b80ad922b0397c95fc
Fons Leenaars <fonsleenaars@gmail.com>
2018-07-07 17:50:13 +0200

    refactor: prepare affix equipment for locales
    Because the types of equipment affixes can occur on isn't in any
    locale data, just use some common tags that the frontend can use
    to implement i18n with.

--- tqdb/utils/core.py
+++ tqdb/utils/core.py
@@ -59,45 +59,33 @@ def print_progress(label,  i, end_val, bar_length=20):
 ###########################################################################
 def get_affix_table_type(file_prefix):
     """
-    Retrieve a friendly name for an affix table.
+    Retrieve the minimal prefix for an affix file name.
 
-    This transforms something like 'headmage' into 'Head (Mage)'
+    The result of these is used to i18n the equipment types an affix can
+    occur on in the frontend.
 
     """
-    if file_prefix.startswith('armsmage'):
-        return 'Arms (Mage)'
-    elif file_prefix.startswith('armsmelee'):
-        return 'Arms (Melee)'
-    elif file_prefix.startswith('headmage'):
-        return 'Head (Mage)'
-    elif file_prefix.startswith('headmelee'):
-        return 'Head (Melee)'
-    elif file_prefix.startswith('legmage'):
-        return 'Legs (Mage)'
-    elif file_prefix.startswith('legmelee'):
-        return 'Legs (Melee)'
-    elif file_prefix.startswith('torsomage'):
-        return 'Chest (Mage)'
-    elif file_prefix.startswith('torsomelee'):
-        return 'Chest (Melee)'
-    elif file_prefix.startswith('amulet'):
-        return 'Amulet'
-    elif file_prefix.startswith('ring'):
-        return 'Ring'
-    elif file_prefix.startswith('shield'):
-        return 'Shield'
-    elif file_prefix.startswith('axe'):
-        return 'Axe'
-    elif file_prefix.startswith('bow'):
-        return 'Bow'
-    elif file_prefix.startswith('club'):
-        return 'Club'
-    elif file_prefix.startswith('spear'):
-        return 'Spear'
-    elif file_prefix.startswith('staff'):
-        return 'Staff'
-    elif file_prefix.startswith('sword'):
-        return 'Sword'
+    for prefix in [
+        'armsmage',
+        'armsmelee',
+        'headmage',
+        'headmelee',
+        'legmage',
+        'legmelee',
+        'torsomage',
+        'torsomelee',
+        'amulet',
+        'ring',
+        'shield',
+        'axe',
+        'bow',
+        'club',
+        'spear',
+        'staff',
+        'sword',
+    ]:
+        if file_prefix.startswith(prefix):
+            return prefix
 
 
 def pluck(d, *k):
